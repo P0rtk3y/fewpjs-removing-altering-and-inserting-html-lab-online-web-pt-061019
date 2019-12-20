cc9f989bad9b26d35f6f015cba9505bed69220f7
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-12-20 02:00:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@
 main.remove()
 let newHeader = document.createElement("h1");
 newHeader.setAttribute("id", "victory");
-newHeader.innerHMTL = "YOUR NAME is the champion"
+newHeader.innerHTML = "YOUR NAME is the champion"
